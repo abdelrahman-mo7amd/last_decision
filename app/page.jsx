f43d5ea15bd80d5b020b097904cd44d5f00334b6
Rehan-thareja <tharejarehan0@gmail.com>
2026-08-15 00:29:05 +0300

    settled up the dashboard page
    made it for abdul

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       let body = document.getElementById("body")
       body.innerHTML = `
       <div>
-    <a href = "/start.jsx" class="hello">Start</a>
+    <a href = "/start" class="hello">Start</a>
    </div>
       `
     }, 3000);
---
+++ app/start/page.jsx
@@ -0,0 +1,16 @@
+export default function Start(){
+    return(
+        <>
+        {/* these are the three*/}
+        <div className="nav">
+
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="fotter">
+
+        </div>
+        </>
+    )
+}
