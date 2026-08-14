eb23497bee6311f3f0988b75b0ff02f8cdd85baa
Rehan-thareja <tharejarehan0@gmail.com>
2026-08-15 02:30:04 +0300

    made another page

---
+++ app/military/page.jsx
@@ -0,0 +1,29 @@
+import Card from "@/components/card"
+export default function Military(){
+    return(
+        <div className="flex flex-col bg-red-600 pt-90 pr-130 pb-90 pl-130 justify-start gap-15">   
+        <div className="flex justify-start items-center gap-15">
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+        </div>  
+        <div className="flex justify-start items-center gap-15">
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+        </div>  
+        <div className="flex justify-start items-center gap-15">
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+        </div>    
+        <div className="flex justify-start items-center gap-15">
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+            <Card text ="asd;fjasdf" img="Img" />
+        </div>  
+
+        
+        </div>
+    )
+}
